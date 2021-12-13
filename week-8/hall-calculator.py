c214f368077dbace21bca8d2acfe2f392ba6ff11
Keith Hall <hkeith62@gmail.com>
2021-12-12 21:37:49 -0600

    Assignment 8.3- Python in action.

---
+++ week-8/hall-calculator.py
@@ -0,0 +1,13 @@
+def hello_world(fname, lname):
+  return "You are now in " + fname + lname + " world"
+def add(x, y):
+  return x + y
+def subtract(x, y):
+  return x - y
+def divide(x, y):
+  return x / y
+
+print(hello_world("Keith", " Hall's"))
+print(add(3, 5))
+print(subtract(35, 5))
+print(divide(30, 5))
